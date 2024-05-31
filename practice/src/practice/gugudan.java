4297afc16726c460616e5b6446b1ade1cbff2718
do <ldh106582@naver.com>
2024-05-31 23:45:23 +0900

    plus

--- practice/src/practice/gugudan.java
+++ practice/src/practice/gugudan.java
@@ -4,7 +4,7 @@ public class gugudan
 {
 	public static void main(String[] args) 
 	{
-		//  문제 : 구구단 만들기
+		//  도현_문제 : 구구단 만들기
 		for(int i = 0; i <= 9; i++ ) 
 		{
 			for(int j = 0; j <= 9; j++) 
@@ -12,5 +12,6 @@ public class gugudan
 				System.out.println(i + "*" + j + "=" + i*j);
 			}
 		}
+		
 	}
 }
