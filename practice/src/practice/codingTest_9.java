202051e58f30c2434b6e1857c2749973dd1ad1db
do <ldh106582@naver.com>
2024-06-11 07:31:39 +0900

    문자열다루기기본~ing

---
+++ practice/src/practice/codingTest_9.java
@@ -0,0 +1,33 @@
+package practice;
+import java.util.*;
+
+public class codingTest_9 
+{
+	public static boolean codingTest_9(String s) 
+	{
+		boolean answer = true;
+		int a = 0;
+		int result = 0;
+		String[] x = s.split("");
+		System.out.println();
+		
+		for(int i = 0; i < s.length(); i++) 
+		{
+			
+		}
+		
+		return answer;
+	}
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("값을 입력해주세요");
+		String s = sc.nextLine();
+		System.out.println("입력하신 값은 : " + s);
+		System.out.println("=======================================");
+		System.out.println("정답 : " + codingTest_9(s));
+		
+	}
+
+}
