a19c27560073f50937569433d0454546a1769a58
do <ldh106582@naver.com>
2024-06-12 07:10:43 +0900

    문자열 다루기 기본

--- practice/src/practice/codingTest_9.java
+++ practice/src/practice/codingTest_9.java
@@ -6,16 +6,22 @@ public class codingTest_9
 	public static boolean codingTest_9(String s) 
 	{
 		boolean answer = true;
-		int a = 0;
-		int result = 0;
-		String[] x = s.split("");
-		System.out.println();
 		
-		for(int i = 0; i < s.length(); i++) 
+		//  길이가 4 혹은 6일 경우를 반환
+		if(s.length() == 4 || s.length() == 6)
 		{
-			
+			// 반복문
+			for(int i = 0; i < s.length(); i++) 
+			{
+				// 문자열열인지 안에 들어오도록 함.
+				// 아스키 코드가 숫자가 48부터 이며 문자열은 57까지
+				if(s.charAt(i) < 48 || s.charAt(i) > 57) 
+				{
+					answer = false;
+					return answer;
+				}
+			}
 		}
-		
 		return answer;
 	}
 
@@ -25,6 +31,13 @@ public class codingTest_9
 		System.out.println("값을 입력해주세요");
 		String s = sc.nextLine();
 		System.out.println("입력하신 값은 : " + s);
+		System.out.println("=======================================");
+		
+		/*
+		 * for(int i = 0; i < s.length(); i++) { char c = s.charAt(i); int asci = (int)
+		 * c; System.out.println("문자열 번호 : " + asci); }
+		 */
+		
 		System.out.println("=======================================");
 		System.out.println("정답 : " + codingTest_9(s));
 		
