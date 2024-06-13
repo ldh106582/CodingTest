1cf8d8196e993c8cbff18da57ff75bce12cfb97f
do <ldh106582@naver.com>
2024-06-14 07:14:46 +0900

    소수찾기 못품ㅠㅠ

---
+++ practice/src/practice/codingTest_11.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class codingTest_11 
+{
+	public static int gpt_10(int n) 
+	{
+		int answer = 0;
+		
+		for(int i = 0; i < n; i++) 
+		{
+			if() 
+			{
+				
+			}
+		}
+		
+		return answer;
+	}
+
+	public static void main(String[] args) 
+	{
+		int n = 10;
+		
+		System.out.println(gpt_10(n));
+
+	}
+
+}
