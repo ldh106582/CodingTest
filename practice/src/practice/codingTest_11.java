3b381576f21f0d895c0e7415937f6c18e7883aa7
do <ldh106582@naver.com>
2024-06-15 10:01:22 +0900

    소수문제 어려움...

--- practice/src/practice/codingTest_11.java
+++ practice/src/practice/codingTest_11.java
@@ -2,27 +2,27 @@ package practice;
 
 public class codingTest_11 
 {
-	public static int gpt_10(int n) 
+	public static boolean gpt_10(int n) 
 	{
-		int answer = 0;
-		
-		for(int i = 0; i < n; i++) 
-		{
-			if() 
-			{
-				
-			}
-		}
-		
-		return answer;
+        if (n <= 1) {
+            return false;
+        }
+        
+        // 2부터 sqrt(num)까지 나눠서 나머지가 0인지 검사
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; // 나누어 떨어지면 소수가 아님
+            }
+        }
+        
+        return true; // 나누어 떨어지지 않으면 소수
 	}
 
 	public static void main(String[] args) 
 	{
 		int n = 10;
 		
-		System.out.println(gpt_10(n));
-
+		boolean isPrime = gpt_10(n);
 	}
 
 }
