08917495591df7fbfce0acdc8157c953ed919b2b
do <ldh106582@naver.com>
2024-06-20 06:57:40 +0900

    이상한 문자 만들기

---
+++ practice/src/practice/codingTest_16.java
@@ -0,0 +1,42 @@
+package practice;
+
+public class codingTest_16 
+{
+
+	public static String gpt_16(String s) 
+	{
+		String answer = "";
+		String[] arr = s.split("");
+		int index = 0;
+		
+		for(String s_answer : arr) 
+		{	
+			if(s_answer.contains(" ")) 
+			{
+				index = 0;
+			}
+			else 
+			{
+				index ++;
+			}
+			if(index % 2 == 0) 
+			{
+				answer += s_answer.toLowerCase();
+			}
+			else 
+			{
+				answer += s_answer.toUpperCase();
+			}
+			System.out.println("answer : " + answer);
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) 
+	{
+		String s = "try hello world";
+		gpt_16(s);
+	}
+
+}
