5317ecf79ebeab1c9025b5f809ed2d63b7d3a18c
do <ldh106582@naver.com>
2024-06-30 08:32:48 +0900

    최소공배수와 최대공약수 ing

--- practice/src/practice/codingTest_23.java
+++ practice/src/practice/codingTest_23.java
@@ -2,11 +2,19 @@ package practice;
 
 public class codingTest_23 
 {
+	
+	public static int gpt_23(int[] n, int m[]) 
+	{
+		int answer = 0;
+		return answer;
+	}
 
 	public static void main(String[] args) 
 	{
 		int[] n = new int[3];
 		int[] m = new int[12];
+		
+		gpt_23(n,m);
 
 	}
 
