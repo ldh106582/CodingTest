ed8877ff56b45c220edafd5b9fa5129c3a5f1dd3
do <ldh106582@naver.com>
2024-07-03 06:27:58 +0900

    최대공약수와 최소공배수

--- practice/src/practice/codingTest_23.java
+++ practice/src/practice/codingTest_23.java
@@ -1,22 +1,31 @@
 package practice;
+import java.util.*;
 
 public class codingTest_23 
 {
 	
-	public static int[] gpt_23(int[] n, int m[]) 
+	public static int[] gpt_23(int n, int m) 
 	{
 		int[] answer = new int[2];
 		
+		int a = Math.max(n, m);
+		int b = Math.min(n, m);
 		
+		while(b != 0) 
+		{
+			int r= a%b;
+			a = b;
+			b = r;
+		}
 		
-		return answer;
+		return new int[] { a, n * m / a };
 	}
 
 	public static void main(String[] args) 
 	{
 		// 두 가의 값을 파라미터로 넘김
-		int[] n = new int[3];
-		int[] m = new int[12];
+		int n = 3;
+		int m = 12;
 		
 		gpt_23(n,m);
 
