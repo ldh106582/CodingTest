75f41e304a9de206cae8bc80bbd7ee4a18d008e3
do <ldh106582@naver.com>
2024-07-07 09:25:11 +0900

    문줖ㄹ이

--- practice/src/practice/codingTest_24.java
+++ practice/src/practice/codingTest_24.java
@@ -6,15 +6,18 @@ public class codingTest_24
 	public static int gpt_24(long num) 
 	{
 		int answer = 0;
-		
+		// 1이 아닐 경우 반복
 		while(num != 1) 
 		{
+			// 나머지가 0이 면
 			if(num % 2 == 0) 
 			{
+				// 2로 나눔 num에 덮어씌움
 				num/=2;
 			}
 			else
 			{
+				// 나머지가 0이 아닐 경우 num * 3 + 1 값
 				num = num * 3 + 1;		
 				answer ++;
 			}
