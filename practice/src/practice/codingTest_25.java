bf3a3f6e0ab7a24073f5144609329dca5328138f
do <ldh106582@naver.com>
2024-07-10 06:47:10 +0900

    평균구하기

---
+++ practice/src/practice/codingTest_25.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class codingTest_25 
+{
+
+	public static double gpt_25(int[] arr) 
+	{
+		double answer = 0;
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) 
+	{
+		int[] arr = new int[] {5,5};
+		
+		System.out.println(gpt_25(arr));
+	}
+
+}
