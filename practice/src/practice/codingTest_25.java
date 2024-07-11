448caa95e03fea4b64ed22383b6f83a79ecade39
do <ldh106582@naver.com>
2024-07-12 06:33:45 +0900

    평균구하기 - 데이터타입변경

--- practice/src/practice/codingTest_25.java
+++ practice/src/practice/codingTest_25.java
@@ -6,12 +6,15 @@ public class codingTest_25
 	public static double gpt_25(int[] arr) 
 	{
 		double answer = 0;
-		int sum = 0;
+		double sum = 0;
 				
 		for(int i = 0; i < arr.length; i++) 
 		{
 			sum += arr[i];
+			System.out.println("arr[i] : " + arr[i]);
+			System.out.println("sum : " + sum);
 			answer = sum/arr.length;
+			System.out.println("answer : " + answer);
 
 		}
 		
@@ -22,7 +25,7 @@ public class codingTest_25
 	public static void main(String[] args) 
 	{
 		// 5,5 를 선언함
-		int[] arr = new int[] {5,5};
+		int[] arr = new int[] {1,2,3,4};
 		
 		System.out.println(gpt_25(arr));
 	}
