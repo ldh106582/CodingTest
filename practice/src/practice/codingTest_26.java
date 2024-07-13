7c89f9fa6933abe3255bac1b63dc276fdfe06a50
do <ldh106582@naver.com>
2024-07-13 11:47:30 +0900

    하샤드 수

---
+++ practice/src/practice/codingTest_26.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class codingTest_26 
+{
+	
+	public static boolean gpt_26(int x) 
+	{
+		boolean answer = true;
+		
+		
+		
+		return answer;
+	}
+
+	public static void main(String[] args) 
+	{
+		int x = 10;
+		
+		gpt_26(x);
+
+	}
+
+}
