7ffb3c173a3f355238f253dec46344cfbd740a8d
do <ldh106582@naver.com>
2024-07-14 09:45:18 +0900

    하샤드 수

--- practice/src/practice/codingTest_26.java
+++ practice/src/practice/codingTest_26.java
@@ -7,14 +7,28 @@ public class codingTest_26
 	{
 		boolean answer = true;
 		
+		int b = x / 100;
+		System.out.println("b : " + b);
+		int c = x / 1000;
+		System.out.println("c : " + c);
+		int d = x / 10000;
+		System.out.println("d : " + d);
 		
+		int a = x % 10;
+		System.out.println("a : " + a);
+	
+		int sum = a + b + c + d;
+		System.out.println("sum : " + sum);
 		
+		if(sum != 0) answer = false;
+		
+		System.out.println("answer : " + answer);
 		return answer;
 	}
 
 	public static void main(String[] args) 
 	{
-		int x = 10;
+		int x = 25;
 		
 		gpt_26(x);
 
