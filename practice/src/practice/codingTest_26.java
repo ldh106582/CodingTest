409a267ca314c4a03bfe89f00d72e706e5a39bac
do <ldh106582@naver.com>
2024-07-17 07:01:49 +0900

    하샤드수 문제 완료

--- practice/src/practice/codingTest_26.java
+++ practice/src/practice/codingTest_26.java
@@ -7,23 +7,18 @@ public class codingTest_26
 	{
 		boolean answer = true;
 		
-		int b = x / 100;
-		System.out.println("b : " + b);
-		int c = x / 1000;
-		System.out.println("c : " + c);
-		int d = x / 10000;
-		System.out.println("d : " + d);
-		
-		int a = x % 10;
-		System.out.println("a : " + a);
-	
-		int sum = a + b + c + d;
+		int a = x / 10000;
+        int b = (x / 1000) % 10;
+		int c = (x / 100) % 10;
+		int d = (x / 10) % 10;
+	    int e = x % 10;
+        
+		int sum = a + b + c + d + e;
 		System.out.println("sum : " + sum);
 		
-		if(sum != 0) answer = false;
+		if(x % sum != 0) answer = false;
 		
 		System.out.println("answer : " + answer);
-		// 12넣었을 때 true 가 나와야 하는데 false가 나옴
 		return answer;
 	}
 
