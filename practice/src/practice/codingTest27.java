cfc61868e49db4047a8d050099f7420b422f8189
do <ldh106582@naver.com>
2024-07-18 06:45:53 +0900

    핸드폰 번호 가리기

---
+++ practice/src/practice/codingTest27.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class codingTest27 
+{
+	public static String gpt_27(String phone_number) 
+	{
+		String asnwer = "";
+		System.out.println(phone_number);
+		
+		return asnwer;
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		String phone_number = "01093341065";
+		
+		gpt_27(phone_number);
+	}
+
+}
