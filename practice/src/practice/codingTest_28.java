f443c6aa9eb16b379eb175f823c86f6b83591136
do <ldh106582@naver.com>
2024-07-21 18:11:05 +0900

    문제ing

--- practice/src/practice/codingTest_28.java
+++ practice/src/practice/codingTest_28.java
@@ -4,15 +4,24 @@ public class codingTest_28
 {
 	public static int[][] gpt_28(int[][] arr1, int[][] arr2)
 	{
-		int [][] answer = {};
+		int [][] answer = new int[arr1.length][arr2.length];
 		
+		for(int i = 0; i < arr1.length; i++)
+		{
+			for(int j = 0; i < arr2.length; i++)
+			{
+					
+			answer[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+		System.out.println(answer);
 		return answer;
 	}
 
-	public static void main(String[] args) 
-	{
-		int[][] arr1 = new int[][] {};
-		int[][] arr2 = new int[][] {};
+	public static void main(String[] args) {
+	
+		int[][] arr1 = {{1,2},{2,3}};
+		int[][] arr2 = {{3,4},{5,6}};
 		
 		gpt_28(arr1, arr2);
 	}
