35453661230d4859e0c7f17a38a2a4c1c5d42603
do <ldh106582@naver.com>
2024-07-23 06:50:50 +0900

    [200~x만큼 간격이 있는 n개의 숫자

---
+++ practice/src/practice/codingTest_29.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class codingTest_29 
+{
+	public static long[] gpt(int x, int n) {
+		long[] answer = {};
+		
+		return answer;
+	}
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
