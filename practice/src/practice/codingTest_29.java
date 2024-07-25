8caade9c874fa5077e0259325225378262c33982
do <ldh106582@naver.com>
2024-07-26 07:12:41 +0900

    문제 풀기

--- practice/src/practice/codingTest_29.java
+++ practice/src/practice/codingTest_29.java
@@ -2,7 +2,7 @@ package practice;
 
 public class codingTest_29 
 {
-	public static long[] gpt(int x, int n) {
+	public static long[] gpt_29(int x, int n) {
 		long[] answer = {};
 		
 		return answer;
@@ -11,6 +11,10 @@ public class codingTest_29
 	public static void main(String[] args) 
 	{
 		// 문제풀이
+		int x = 0;
+		int n = 0;
+		
+		gpt_29(x, n);
 	}
 
 }
