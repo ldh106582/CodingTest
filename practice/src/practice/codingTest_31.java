eecd68a133f0fc9719a78bab7d44fe9c6905248f
do <ldh106582@naver.com>
2024-08-01 06:25:37 +0900

    소수만들기

--- practice/src/practice/codingTest_31.java
+++ practice/src/practice/codingTest_31.java
@@ -2,10 +2,20 @@ package practice;
 
 public class codingTest_31 
 {
+	
+	public static int gpt_31(int[] nums) 
+	{
+		int answer = -1;
+		
+		return answer;
+	}
 
 	public static void main(String[] args) 
 	{
-
+		int[] nums = new int[] {1,2,3,4};
+		
+		gpt_31(nums)
+		
 	}
 
 }
