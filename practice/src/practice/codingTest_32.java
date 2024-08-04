dc49d77e8c6593ff2dfb825c5c62c245081b8e48
do <ldh106582@naver.com>
2024-08-04 10:40:23 +0900

    예산

---
+++ practice/src/practice/codingTest_32.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class codingTest_32 
+{
+	
+	public static int gpt_32(int[] d, int budget) 
+	{
+		int answer = 0;
+		
+		return answer;
+	}
+
+	public static void main(String[] args) 
+	{
+		int [] d = new int[] {1,3,2,5,4};
+		int budget = 9;
+		
+		gpt_32(d, budget);
+	}
+
+}
