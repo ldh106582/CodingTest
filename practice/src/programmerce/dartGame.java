40f0efe718977648ff2d944b3fd412ef01df636a
do <ldh106582@naver.com>
2024-08-11 09:48:47 +0900

    [1차] 다트게임

---
+++ practice/src/programmerce/dartGame.java
@@ -0,0 +1,19 @@
+package programmerce;
+
+
+public class dartGame 
+{
+	public static int dart_Game(String dartResult) 
+	{
+		int answer = 0;
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) 
+	{
+		String dartResult = null;
+		dart_Game(dartResult);
+	}
+
+}
