c328787849734029ebbd1d97966440ecba91925d
do <ldh106582@naver.com>
2024-08-14 06:31:13 +0900

    1차 다트 문제 풀이

--- practice/src/programmerce/dartGame.java
+++ practice/src/programmerce/dartGame.java
@@ -6,7 +6,17 @@ public class dartGame
 	public static int dart_Game(String dartResult) 
 	{
 		int answer = 0;
+		
+		String[] dart = dartResult.split("");
 		int[] srcore = new int[3];
+		
+		int index = -1; // 스코어 인덱스번호
+		// 2024.08.14 문제 풀이
+		for(int i = 0; i < dart.length; i++) 
+		{
+			index++;
+		}
+		
 		
 		return answer;
 	}
@@ -14,7 +24,7 @@ public class dartGame
 	public static void main(String[] args) 
 	{
 		// 2024.08.13 1차 문제 풀이
-		String dartResult = null;
+		String dartResult = "1S2D*3T";
 		dart_Game(dartResult);
 	}
 
