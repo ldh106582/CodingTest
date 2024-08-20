05ef55eb77982bf8409c7b8cae38258c3aca091d
do <ldh106582@naver.com>
2024-08-21 07:01:11 +0900

    race 게임

---
+++ practice/src/programmerce/race.java
@@ -0,0 +1,20 @@
+package programmerce;
+
+public class race 
+{
+	public static String race(String[] participant, String[] completion) 
+	{
+		String answer = null;
+		
+		return answer;
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		String[] participant = new String[] {"leo", "kiki", "eden"};
+		String[] completion = new String[] {"kiki", "eden"};
+		race(participant, completion);
+	}
+
+}
