8a2803ae5c52e9f43a15146266c68bb97747e464
do <ldh106582@naver.com>
2024-08-22 07:01:00 +0900

    완주하지 못한 선수

--- practice/src/programmerce/race.java
+++ practice/src/programmerce/race.java
@@ -1,11 +1,32 @@
 package programmerce;
 
+import java.util.Arrays;
+
 public class race 
 {
 	public static String race(String[] participant, String[] completion) 
 	{
+		
+		// 오류발생
+//		Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2
+//		at Solution.solution(Unknown Source)
+//		at SolutionTest.lambda$main$0(Unknown Source)
+//		at SolutionTest$SolutionRunner.run(Unknown Source)
+//		at SolutionTest.main(Unknown Source)
+		
 		String answer = null;
 		
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		
+		for(int i = 0; i < participant.length; i++)
+		{
+			if(!participant[i].equals(completion[i])) 
+			{
+				answer = participant[i];
+				break;
+			}
+		}
 		return answer;
 	}
 	
