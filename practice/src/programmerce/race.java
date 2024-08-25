2bd25c93ddd52bbd6bd69ab4e87650e4c6b80d2c
do <ldh106582@naver.com>
2024-08-26 06:53:11 +0900

    완주하지 못한 선수 새로운 풀이

--- practice/src/programmerce/race.java
+++ practice/src/programmerce/race.java
@@ -1,33 +1,36 @@
 package programmerce;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class race 
 {
 	public static String race(String[] participant, String[] completion) 
 	{
 		
-		// 오류발생
-//		Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2
-//		at Solution.solution(Unknown Source)
-//		at SolutionTest.lambda$main$0(Unknown Source)
-//		at SolutionTest$SolutionRunner.run(Unknown Source)
-//		at SolutionTest.main(Unknown Source)
-		
-		String answer = null;
-		
 		Arrays.sort(participant);
 		Arrays.sort(completion);
 		
-		for(int i = 0; i < completion.length; i++)
+		int i = 0;
+		for(i = 0; i < completion.length; i++)
 		{
 			if(!participant.equals(completion))
 			{
-				answer = completion[i];
-				break;
+				return participant[i];
 			}
 		}
-		return answer;
+		return participant[i];
+	}
+	
+	public static String race2(String[] participant, String[] completion) 
+	{
+		String answer = "";
+		
+		HashMap<String, Integer> hm = new HashMap<>();
+		for(String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+		
+		
+		return null;
 	}
 	
 	
@@ -36,20 +39,8 @@ public class race
 
 		String[] participant = new String[] {"marina", "josipa", "nikola", "vinko", "filipa"};
 		String[] completion = new String[] {"josipa", "filipa", "marina", "nikola"};
-		String answer = null;
 		
-		Arrays.sort(participant);
-		Arrays.sort(completion);
-		
-		int i = 0;
-		for(i = 0; i < completion.length; i++ ) 
-		{
-			if(!participant[i].equals(completion[i])) 
-			{
-                participant[i];
-			}
-		}
-		
+		race(participant, completion);
 	}
 
 }
