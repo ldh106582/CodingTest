26096c7f061f5519c5c59ea820dd1b93c0ffb32b
do <ldh106582@naver.com>
2024-08-27 06:31:00 +0900

    완주하지 못한 선수 새로운 풀이

--- practice/src/programmerce/race.java
+++ practice/src/programmerce/race.java
@@ -28,7 +28,13 @@ public class race
 		
 		HashMap<String, Integer> hm = new HashMap<>();
 		for(String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+		for(String player : completion) hm.put(player, hm.get(player) - 1);
 		
+		for(String key : hm.keySet()) {
+			if(hm.get(key) != 0) {
+				answer = key;
+			}
+		}
 		
 		return null;
 	}
