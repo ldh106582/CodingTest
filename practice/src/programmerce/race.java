1ad9059a4a8f51c98096b291d57493bae64d4422
do <ldh106582@naver.com>
2024-08-28 19:43:46 +0900

    완료"
    :q
    git commit -m 완료"

--- practice/src/programmerce/race.java
+++ practice/src/programmerce/race.java
@@ -30,13 +30,14 @@ public class race
 		for(String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
 		for(String player : completion) hm.put(player, hm.get(player) - 1);
 		
-		for(String key : hm.keySet()) {
-			if(hm.get(key) != 0) {
+		for(String key : hm.keySet()) 
+		{
+			if(hm.get(key) != 0) 
+			{
 				answer = key;
 			}
 		}
-		
-		return null;
+		return answer;
 	}
 	
 	
