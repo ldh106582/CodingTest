109397b7d9b71d6b8365743f17c8d091869e22e3
do <ldh106582@naver.com>
2024-09-06 06:44:18 +0900

    sampleTest

---
+++ practice/src/programmerce/sampleTest.java
@@ -0,0 +1,21 @@
+package programmerce;
+
+public class sampleTest 
+{
+	public static int[] solution(int answers[])
+	{
+		int[] answer = {};
+		
+		return answer;
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		int[] answers = new int[] {};
+		
+		solution(answers);
+		
+	}
+
+}
