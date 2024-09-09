f1f3d6658367f6cc8dc61575af47f6fb9f92bb0c
do <ldh106582@naver.com>
2024-09-10 06:58:39 +0900

    정보처리기사 문제 풀이

---
+++ practice/src/practice/j_03.java
@@ -0,0 +1,28 @@
+package practice;
+
+class point{
+	int x;
+	int y;
+}
+
+public class j_03 
+{
+
+	public static void main(String[] args) 
+	{
+		point m = new point();
+		m.x= 1000;
+		calc(m);
+		getValue(m);
+		System.out.printf("%d",m.y);
+	}
+	
+	static void calc(point s) {
+		s.x /= 10;
+	}
+	
+	static void getValue(point p) {
+		p.y = p.x;
+	}
+
+}
