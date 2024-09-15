235e05ee5b99a35f1384687d76871914bf04e042
do <ldh106582@naver.com>
2024-09-15 10:44:53 +0900

    배열 형식 맞춰 보기

--- practice/src/practice/test01.java
+++ practice/src/practice/test01.java
@@ -1,14 +1,25 @@
 package practice;
 
+import java.util.*;
+
 public class test01 
 {
 
 	public static void main(String[] args) 
 	{
 		String[] a = new String[]{"도현", "승희", "철수"};
-		String[] a = new String[]{"철수", "도현", "승희"};
-
+		String[] b = new String[]{"철수", "도현", "승희"};
 		
+		Map<String, Integer> map = new HashMap<>();
 		
+		for(int i = 0; i < a.length; i++) {
+			map.put(a[i], i);
+		}
+		
+		Arrays.sort(b, (before, After) -> {
+			return Integer.compare(map.get(before), map.get(After));
+		});
+		
+	     System.out.println("재배치된 배열: " + Arrays.toString(b));
 	}
 }
