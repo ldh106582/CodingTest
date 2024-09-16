23603b3e8da92d96aeb959e62039285ebfeaadaa
do <ldh106582@naver.com>
2024-09-17 06:33:27 +0900

    map에 대한 추가 공부

--- practice/src/practice/test01.java
+++ practice/src/practice/test01.java
@@ -10,11 +10,15 @@ public class test01
 		String[] a = new String[]{"도현", "승희", "철수"};
 		String[] b = new String[]{"철수", "도현", "승희"};
 		
+		// 인덱스를  map에 저장하여 index 값을 매핑하기 위해 생성
 		Map<String, Integer> map = new HashMap<>();
 		
 		for(int i = 0; i < a.length; i++) {
 			map.put(a[i], i);
+			// 인덱스 번호를 map에 저장함.
+			System.out.println(map.get(a[i]));
 		}
+	
 		
 		Arrays.sort(b, (before, After) -> {
 			return Integer.compare(map.get(before), map.get(After));
