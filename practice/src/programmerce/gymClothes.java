3c74d0f9842585754fa735d70d64b7a06cb52391
do <ldh106582@naver.com>
2024-09-18 17:21:39 +0900

    체육복

---
+++ practice/src/programmerce/gymClothes.java
@@ -0,0 +1,23 @@
+package programmerce;
+
+public class gymClothes 
+{
+	public int solution(int n, int[] lost, int[] reverse) 
+	{
+		int answer = 0;
+		
+		return answer;
+	}
+	
+
+	public static void main(String[] args) 
+	{
+		int n = 5;
+		int[] lost = new int[] {2,4};
+		int[] reverse = new int[] {1,3,5};		
+		gymClothes gym = new gymClothes();
+		
+		gym.solution(n ,  lost, reverse)
+	}
+
+}
