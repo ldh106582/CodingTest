94440340ae0fc84b593272b8bbe953c0508e2611
do <ldh106582@naver.com>
2024-09-19 07:02:00 +0900

    체육복문제

--- practice/src/programmerce/gymClothes.java
+++ practice/src/programmerce/gymClothes.java
@@ -1,10 +1,14 @@
 package programmerce;
+import java.util.*;
 
 public class gymClothes 
 {
 	public int solution(int n, int[] lost, int[] reverse) 
 	{
 		int answer = 0;
+		
+		System.out.println(Arrays.toString(lost));
+		System.out.println(Arrays.toString(reverse));
 		
 		return answer;
 	}
@@ -17,7 +21,7 @@ public class gymClothes
 		int[] reverse = new int[] {1,3,5};		
 		gymClothes gym = new gymClothes();
 		
-		gym.solution(n ,  lost, reverse)
+		gym.solution(n ,  lost, reverse);
 	}
 
 }
