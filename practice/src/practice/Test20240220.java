f1289b6c8b490ef6ab8b65cc5e5d36559584ebc4
do <ldh106582@naver.com>
2024-09-23 06:43:55 +0900

    202402회차 20번 문제 풀이

---
+++ practice/src/practice/Test20240220.java
@@ -0,0 +1,19 @@
+package practice;
+import java.util.*;
+
+
+public class Test20240220 
+{
+
+	public static void main(String[] args) 
+	{
+		String str = "ITISTESTSTRING";
+		String[] result = str.split("T");
+		System.out.println(result[3]);
+		System.out.println(Arrays.toString(result));
+		for(int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+
+}
