4b00c4101c9007b0d6ea6c8b94085eb20caa4dff
do <ldh106582@naver.com>
2024-09-25 06:45:47 +0900

    정보처리기사문제 풀이

---
+++ practice/src/practice/Test_20240206.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Test_20240206 
+{
+
+	public static void main(String[] args) 
+	{
+		String a = "abdcabcabca";
+		String pa1 = "ab";
+		String p2 = "ca";
+
+	}
+
+}
