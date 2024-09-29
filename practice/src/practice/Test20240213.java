cc2d146af7fb9d5bdfe4e3eedeccfc6caeb17160
do <ldh106582@naver.com>
2024-09-30 06:16:39 +0900

    정보처리기사 문제 풀이

---
+++ practice/src/practice/Test20240213.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Test20240213 
+{
+
+	public static void main(String[] args) 
+	{
+		int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+		
+        int[] parr1 = arr[1]; // arr[1]에 해당하는 배열
+        int[] parr2 = arr[2]; // arr[2]에 해당하는 배열
+        
+        // C 코드의 계산식과 동일하게 계산
+        int result = parr2[1] + parr2[2] + parr1[0];
+        
+        System.out.println(result);
+	}
+
+}
