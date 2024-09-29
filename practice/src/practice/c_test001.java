f6fbfde82db21ec184ce2a56678fb5b93a151457
do <ldh106582@naver.com>
2024-09-29 18:21:49 +0900

    정보처리기사공부

--- practice/src/practice/c_test001.java
+++ practice/src/practice/c_test001.java
@@ -14,23 +14,23 @@ public class c_test001
 		
 		for (int i = 0; i < 100; i++)
 		{
-			if(a == b) { System.out.println(i + "번 : " +"Yes"); };
+			if(a == b) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");};
 			i++;
-			if(a.equals(c)) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");	};
+			if(a.equals(c)) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");};
 			i++;
 			// if(a == d) { System.out.println("Yes"); }else { System.out.println("no");	} //no;		
 			
-			if(a.equals(b)) { System.out.println(i + "번 : " +"Yes"); };
+			if(a.equals(b)) { System.out.println(i + "번 : " +"Yes");} else { System.out.println(i + "번 : " +"no"); };
 			i++;
-			if(c.equals(d)) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");	};
+			if(c.equals(d)) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");};
 			i++;
 			if(c == d) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");	};
 			i++;
-			if(e.equals(f)) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");	};
+			if(e.equals(f)) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");};
 			i++;
 			if(e == f) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");	};
 			i++;
-			if(a.equals(f)) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");	};
+			if(a.equals(f)) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");};
 			i++;
 			if(a == f) { System.out.println(i + "번 : " +"Yes"); } else { System.out.println(i + "번 : " +"no");	};
 			i++;
