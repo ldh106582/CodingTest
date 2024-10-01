21917595faa292f319c3b4c4a4478c7d2cdc3261
do <ldh106582@naver.com>
2024-10-02 06:45:55 +0900

    정보처리기사 substring 공부

--- practice/src/practice/javaTest.java
+++ practice/src/practice/javaTest.java
@@ -7,18 +7,23 @@ public class javaTest
 	{
 		String base = "Hello EunWoo";
 		char ch = 'o';
+		String a = base.substring(1);
+//		System.out.println(a);
 		for (int i = 0; i < base.length(); i++) 
 		{
 			if(ch == base.charAt(i))
 			{
 				base = base.substring(0,i) + '*' + base.substring(i+1);
-			System.out.println(base.substring(0,i));
+//			System.out.println(base.substring(0,i));
 //			System.out.println(base.substring(i + 1));
+//				System.out.println(base);
 			}
 			else 
 				base = base.substring(0,i) + '-' + base.substring(i+1);
+//			System.out.println(base.substring(0,i));
+//			System.out.println(base.substring(i + 1));
 		}
-//		System.out.println(base);
+		System.out.println(base);
 	}
 
 }
