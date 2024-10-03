31083182a9563bb1f6f29f74f6a65eb42fb3896f
do <ldh106582@naver.com>
2024-10-03 10:17:52 +0900

    정보처리기사문제 풀이

---
+++ practice/src/practice/j_02.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class j_02 
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 20, b = 100;
+		System.out.printf("%d %d\n", a, b);
+		System.out.printf("%b\n", a >= 20 || ++b == 100);
+		System.out.printf("%d %d\n", a, b);
+
+	}
+
+}
