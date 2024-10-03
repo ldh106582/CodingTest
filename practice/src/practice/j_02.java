da756236c155174308f01edd18bd54b79e6958f8
do <ldh106582@naver.com>
2024-10-04 08:07:03 +0900

    정보처리기사 공부

--- practice/src/practice/j_02.java
+++ practice/src/practice/j_02.java
@@ -9,6 +9,7 @@ public class j_02
 		System.out.printf("%d %d\n", a, b);
 		System.out.printf("%b\n", a >= 20 || ++b == 100);
 		System.out.printf("%d %d\n", a, b);
+		System.out.printf("%d %d\n", a==b);
 
 	}
 
