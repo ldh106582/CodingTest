43429126749e589ea70a31cf4e84ab44c96b6875
do <ldh106582@naver.com>
2024-10-05 08:55:31 +0900

    정보처리기사 풀이

---
+++ practice/src/practice/Test20240217.java
@@ -0,0 +1,24 @@
+package practice;
+
+public class Test20240217 
+{
+
+	public static void main(String[] args) 
+	{
+	   String str = "abacabcd";
+	   boolean[] seen = new boolean[256];
+	   System.out.print(calculFn(str, str.length()-1, seen));
+	   
+	}
+    public static String calculFn(String str, int index, boolean[] seen) {
+        if(index < 0) return "";
+        char c = str.charAt(index);
+        String result = calculFn(str, index-1, seen);
+        if(!seen[c]) {
+            seen[c] = true;
+            return c + result;
+        }
+        return result;
+    }
+
+}
