93654684e1698f4fbd2c2831d604619b7deb5ba2
do <ldh106582@naver.com>
2024-10-06 08:47:56 +0900

    정보처리기사 공부

--- practice/src/practice/Test20240217.java
+++ practice/src/practice/Test20240217.java
@@ -7,13 +7,15 @@ public class Test20240217
 	{
 	   String str = "abacabcd";
 	   boolean[] seen = new boolean[256];
-	   System.out.print(calculFn(str, str.length()-1, seen));
+//	   System.out.print(calculFn(str, str.length()-1, seen));
 	   
 	}
     public static String calculFn(String str, int index, boolean[] seen) {
         if(index < 0) return "";
         char c = str.charAt(index);
+        System.out.println(c);
         String result = calculFn(str, index-1, seen);
+        System.out.println(result);
         if(!seen[c]) {
             seen[c] = true;
             return c + result;
