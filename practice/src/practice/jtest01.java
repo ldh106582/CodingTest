a4fef4e523723576171dcf5b821269a8edf97c9f
do <ldh106582@naver.com>
2024-10-11 06:56:14 +0900

    재귀함수 공부

---
+++ practice/src/practice/jtest01.java
@@ -0,0 +1,30 @@
+package practice;
+
+public class jtest01 
+{
+	
+	public int Obnumber(int a) 
+	{
+		int ab = 0;
+
+		if (a > 1) {
+			Obnumber(a - 1);
+			ab = a % 2;
+			System.out.println("값 : " + a + " 답 : " +  ab);
+		}
+		
+		return ab;
+	}
+
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		
+		jtest01 ob = new jtest01();
+		ob.Obnumber(a);
+		System.out.println("end");
+		
+
+	}
+
+}
