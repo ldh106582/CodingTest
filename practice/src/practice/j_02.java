dbf4b645002b3ca1b0283d6f6dc48d658a8e32d7
do <ldh106582@naver.com>
2024-10-17 07:07:04 +0900

    switch 문 추가 공부

--- practice/src/practice/j_02.java
+++ practice/src/practice/j_02.java
@@ -5,12 +5,24 @@ public class j_02
 
 	public static void main(String[] args) 
 	{
-		int a = 20, b = 100;
-		System.out.printf("%d %d\n", a, b);
-		System.out.printf("%b\n", a >= 20 || ++b == 100);
-		System.out.printf("%d %d\n", a, b);
-		System.out.printf("%b\n", a==b);
-		System.out.println(b);
+//		int a = 20, b = 100;
+//		System.out.printf("%d %d\n", a, b);
+//		System.out.printf("%b\n", a >= 20 || ++b == 100);
+//		System.out.printf("%d %d\n", a, b);
+//		System.out.printf("%b\n", a==b);
+//		System.out.println(b);
+//		
+		int c = 4;
+		switch(++c) 
+		{
+			case 5 : c+=1;
+			break;
+			case 3 : c++;
+			case 4 : c++;
+			break;
+			default : System.out.println("default");;
+		}
+		System.out.println(c);
 	}
 
 }
