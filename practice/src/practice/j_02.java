38a68428c20abe0500ef6ca3a260c1b6e3ed282f
do <ldh106582@naver.com>
2024-10-18 06:47:11 +0900

    switch 공부

--- practice/src/practice/j_02.java
+++ practice/src/practice/j_02.java
@@ -15,10 +15,10 @@ public class j_02
 		int c = 4;
 		switch(++c) 
 		{
-			case 5 : c+=1;
+			case 5 : c+=1; System.out.println("1 번 케이스");
 			break;
-			case 3 : c++;
-			case 4 : c++;
+			case 3 : c++; System.out.println("3 번 케이스");
+			case 4 : c--; System.out.println("4 번 케이스");
 			break;
 			default : System.out.println("default");;
 		}
