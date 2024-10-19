cdfff55400e5c7d2c7cfb7e61db9b6cd1a6acda7
do <ldh106582@naver.com>
2024-10-20 05:54:36 +0900

    for문 재 풀이

---
+++ practice/src/practice/test_05.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class test_05 
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 0;
+		for(int i = 0; i < 100; i *= 2) 
+		{
+			System.out.println("i : " + i);
+			if((++i)%3 == 0) 
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
