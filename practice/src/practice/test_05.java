472cbc29bd5af5e6dcb701028306de0c2ab1f949
do <ldh106582@naver.com>
2024-10-23 07:28:13 +0900

    정보처리기사 풀이

--- practice/src/practice/test_05.java
+++ practice/src/practice/test_05.java
@@ -6,9 +6,11 @@ public class test_05
 	public static void main(String[] args) 
 	{
 		int a = 0;
+		int b = 0;
 		for(int i = 0; i < 100; i *= 2) 
 		{
 			System.out.println("i : " + i);
+			System.out.println(i + b);
 			if((++i)%3 == 0) 
 			{
 				System.out.println((++i)%3);
