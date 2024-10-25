54fee8d285c4b922e0f67523a6126761473dc77a
do <ldh106582@naver.com>
2024-10-25 22:16:32 +0900

    실패울 공부

---
+++ practice/src/programmerce/failpercetage.java
@@ -0,0 +1,20 @@
+package programmerce;
+
+public class failpercetage 
+{
+	public int[] solution(int n, int[] strage) {
+		int[] result = {};
+		
+		return result;
+	}
+
+	public static void main(String[] args) 
+	{
+		int n = 5;
+		int[] stage = new int[] {2,1,2,6,2,4,3,3};
+		failpercetage s = new failpercetage();
+		s.solution(n, stage);
+		
+	}
+
+}
